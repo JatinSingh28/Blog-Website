c4a4c6f9c11d1e76a27f1b73de95d4a91bd10fcd
Jatin Singh <sagoisinghjatin9951@gmail.com>
2022-10-11 01:40:29 +0530

    rm DB

--- project/project/settings.py
+++ project/project/settings.py
@@ -76,16 +76,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    # 'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE' : 'django.db.backends.postgresql',
-        'NAME': 'itpgfkmdytpndbhskxsh',
-        'USER' : 'postgres',
-        'PASSWORD': 'epqNy1YUanny1W1N',
-        'HOST': 'itpgfkmdytpndbhskxsh.db.ap-south-1.nhost.run',
-        'PORT': '5432',
-    }
+
+    #     'ENGINE' : 'django.db.backends.postgresql',
+    #     'NAME': 'itpgfkmdytpndbhskxsh',
+    #     'USER' : 'postgres',
+    #     'PASSWORD': 'epqNy1YUanny1W1N',
+    #     'HOST': 'itpgfkmdytpndbhskxsh.db.ap-south-1.nhost.run',
+    #     'PORT': '5432',
+    # }
 }
 
 

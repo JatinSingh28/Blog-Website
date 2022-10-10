61c429deafd16e875021d4258cfca07e89ffbc64
Jatin Singh <sagoisinghjatin9951@gmail.com>
2022-10-11 01:38:35 +0530

    Vercel

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#%gyx&#=+oai)gd$8z0u(!hmnfycz(_cjf$#v&y(=^^(o!q&a4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.vercel.app']
 
 
 # Application definition
